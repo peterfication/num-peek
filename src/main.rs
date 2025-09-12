a40d635344f8b378934326336c99fd43564d6290
Peter Morgenstern <mail@petergundel.de>
2025-09-12 09:02:59 +0200

    Validate file name and that it exists

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,27 @@ use clap::Parser;
 )]
 struct Cli {
     /// Path to the *.npy file
-    #[arg(value_name = "FILE_PATH")]
+    #[arg(value_name = "FILE_PATH", value_parser = validate_npy)]
     file_path: String,
+}
+
+fn validate_npy(string: &str) -> Result<String, String> {
+    if string.ends_with(".npy") {
+        Ok(string.to_string())
+    } else {
+        Err(String::from("File must have a .npy extension"))
+    }
 }
 
 fn main() {
     let cli = Cli::parse();
+
+    // Make sure the file exists
+    // If not, print an error message and exit
+    if !std::path::Path::new(&cli.file_path).exists() {
+        eprintln!("Error: File '{}' does not exist.", cli.file_path);
+        std::process::exit(1);
+    }
+
     println!("Peek into {}", cli.file_path);
 }
