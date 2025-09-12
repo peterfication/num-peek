fb93d25a7ea9e78210269ad4626c92ae1a3470f6
Peter Morgenstern <mail@petergundel.de>
2025-09-12 09:42:46 +0200

    Create float demo file

--- Justfile
+++ Justfile
@@ -5,6 +5,10 @@ default:
 run FILE_PATH="assets/demo.npy":
   cargo run -- {{FILE_PATH}}
 
-# Create a .npy file using the provided script (used for testing)
+# Create a .npy file with int values using the provided script (used for testing)
 npy_create FILE_PATH="assets/demo.npy":
   uv run ./scripts/npy_create.py {{FILE_PATH}}
+
+# Create a .npy with float values file using the provided script (used for testing)
+npy_create_float FILE_PATH="assets/demo_float.npy":
+  uv run ./scripts/npy_create.py {{FILE_PATH}} --float
--- scripts/npy_create.py
+++ scripts/npy_create.py
@@ -18,20 +18,26 @@ import sys
 import numpy as np
 
 
-def create_npy_file(file_path):
+def create_npy_file(file_path, use_float=False):
     """
     Creates a new .npy file at file_path if it does not exist already.
 
     Args:
         file_path (str): The path to the .npy file.
+        use_float (bool): Whether to use float values in the array.
     """
     if os.path.exists(file_path):
         print(f"Error: File exists already at '{file_path}'", file=sys.stderr)
         sys.exit(1)
 
-    array = np.array([[1, 4, 3], [8, 22, 12]])
+    if use_float:
+        array = np.array([[1.1, 4.2, 3.3], [8.4, 22.5, 12.6]], dtype=float)
+    else:
+        array = np.array([[1, 4, 3], [8, 22, 12]])
 
     np.save(file_path, array)
+
+    print(f"Created .npy file at '{file_path}' with contents:\n{array}")
 
 
 def main():
@@ -43,8 +49,9 @@ def main():
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument("file_path", type=str, help="The full path to ouput .npy file")
+    parser.add_argument("--float", action="store_true", help="Create array with float values")
     args = parser.parse_args()
-    create_npy_file(args.file_path)
+    create_npy_file(args.file_path, use_float=args.float)
 
 
 if __name__ == "__main__":
